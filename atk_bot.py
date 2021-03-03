b75a1d0a9a913b80b09d0a6d88afdc3fa46297ec
makeinstall77 <make_install@yahoo.com>
2021-03-03 14:07:06 +1000

    minor updates

--- atk_bot.py
+++ atk_bot.py
@@ -6,6 +6,9 @@ import requests
 import telebot
 import sys
 import os
+import logging
+import time
+#import streets
 
 #config init
 config = ConfigParser()
@@ -17,13 +20,22 @@ valid_chat = config.get('id', 'valid_chat')
 valid_chat2 = config.get('id', 'valid_chat2')
 save_dir = config.get('vars', 'save_dir')
 user_agent_val = config.get('vars', 'user_agent_val')
+wait_time = int(config.get('vars', 'wait_time'))
 
-request_num = {}
-request_str = {}
-request = {}
+#setup logging
+logging.basicConfig(filename=os.path.basename(sys.argv[0])+'.log', level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
-bot = telebot.TeleBot(bot_id)
-
+#create bot instance
+try:
+    bot = telebot.TeleBot(bot_id)
+except Exception as e:
+    print (e)
+    exc_type, exc_obj, exc_tb = sys.exc_info()
+    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+    logging.error(exc_type, fname, exc_tb.tb_lineno)
+    
+#getting other stuff from config
 url = {
     'root_url' : config.get('url', 'root_url'),
     'ref_url_ref' : config.get('url', 'ref_url_ref'),
@@ -33,12 +45,71 @@ url = {
     'search_url' : config.get('url', 'search_url')
 }
 
+#global vars
+request_num = {}
+request_str = {}
+request = {}
+
 def extract_arg(arg):
     return arg.split(maxsplit=1)[1:]
     
 def get_command(arg):
     return arg.split(' ', 1)[0]
     
+def start_session():
+    _s = requests.Session()
+    _r = _s.get(url.get('root_url'), headers = {'User-Agent': user_agent_val})
+    _cookie = _s.cookies.get(url.get('cookie'), domain=url.get('host'))
+    _s.headers.update({'Referer':url.get('ref_url_ref')})
+    _s.headers.update({'User-Agent':user_agent_val})
+    _r = _s.get(url.get('auth_page'), headers = {'User-Agent': user_agent_val})
+    _p = _s.post(url.get('auth_page'), {
+    'sectok' : '', 
+    'id' : 'start',
+    'do' : 'login',
+    'u' : login,
+    'p' : password,
+    'r' : 1
+    })
+    return _s
+    
+def search_pages(_arg):
+    _s = start_session()
+    _r = _s.get(url.get('search_url') + _arg, headers = {'User-Agent': user_agent_val})
+    _c = _r.content
+    return _c
+    
+def search_files(_src):
+    _s = _s = start_session()
+    _r = _s.get(url.get('root_url') + _src, headers = {'User-Agent': user_agent_val})
+    _c = _r.content
+    
+    return _c
+    
+def get_file(_url):
+    _s = start_session()
+    _r = _s.get(url.get('root_url') + _url, headers = {'User-Agent': user_agent_val})
+    return _r
+    
+def write_file(_n, _h):
+    f = open(_n, "wb")
+    r = get_file(_h)
+    f.write(r.content)
+    f.close()
+    
+def parse_pdf(_c):
+    _files = []
+    soup = BeautifulSoup(_c,'lxml')
+    for var in soup.findAll('a', class_="media mediafile mf_pdf"):
+        _n = var["title"]
+        _h = var['href']
+        _n = ('.pdf'.join(_n.split('.pdf')[:-1]) + '.pdf')
+        _n = _n.replace('_', ' ').replace('corp', '').replace('pld', '')
+        _path = save_dir + _n
+        write_file(_path, _h)
+        _files.append(_path)
+    return _files
+
 @bot.message_handler(content_types=['text'])
 def pld(message):
     global request_num
@@ -46,40 +117,20 @@ def pld(message):
     global request
     _chat_id = message.chat.id
     _command = get_command(message.text).lower()
+    
     try:
         _args = extract_arg(message.text)[0]
     except:
+        _args = ''
         pass
+        
     try:
         if (str(_chat_id) == str(valid_chat) or str(_chat_id) == str(valid_chat2)):
-            if (_command == 'плд'):
-                #bot.reply_to(message, "Ищу плд по имени: " + _args)
-                
-                s = requests.Session()
-                r = s.get(url.get('root_url'), headers = {'User-Agent': user_agent_val})
-                print ("set User Agent: "+user_agent_val)
-                
-                _cookie = s.cookies.get(url.get('cookie'), domain=url.get('host'))
-                s.headers.update({'Referer':url.get('ref_url_ref')})
-                s.headers.update({'User-Agent':user_agent_val})
-                print ("set " + url.get('cookie')+" to "+str(_cookie))
-                       
-                r = s.get(url.get('auth_page'), headers = {'User-Agent': user_agent_val})
-                print ("try to auth on "+url.get('auth_page'))
-                
-                post_request = s.post(url.get('auth_page'), {
-                'sectok' : '', 
-                'id' : 'start',
-                'do' : 'login',
-                'u' : login,
-                'p' : password,
-                'r' : 1
-                })
-                
-                r = s.get(url.get('search_url')+_args, headers = {'User-Agent': user_agent_val})
-                print ("searching "+url.get('search_url')+_args)
-                
-                c = r.content
+            if ((_command == 'плд') and (_args != "")):
+            
+                msg = 'pld search: "' + 'плд ' + _args + '" from: ' + str(_chat_id)
+                logging.warning(msg)
+                c = search_pages(_args)
                 soup = BeautifulSoup(c,'lxml')
                 svars = {}
                 
@@ -90,7 +141,7 @@ def pld(message):
                 msg = ""
                 for key in svars:
                     num += 1
-                    msg += "➡️ " + str(num) + " " + key.replace('corp:pld:', '').replace('_', ' ').replace('corp:','') + "\n"
+                    msg += "➡️ " + str(num) + " " + key.replace('corp:pld:', '').replace('_', ' ').replace('corp:','').replace('pld1:', '').replace('it:', '').replace('tp:', '') + "\n"
                 
                 if (len(msg) > 1023):
                     msg = msg[:1023]
@@ -100,36 +151,26 @@ def pld(message):
                     msg =  "Нашлось совпадений: " + str(num) + "\n\n" + msg
                 
                 if (num == 1):
-                    bot.send_message(_chat_id, msg)
+                    bot.reply_to(message, msg)
                     keys = list(svars.keys())
                     src = svars.get(keys[num-1])
-                    
-                    r = s.get(url.get('root_url') + src, headers = {'User-Agent': user_agent_val})
-                    
-                    c = r.content
-                    soup = BeautifulSoup(c,'lxml')
-                    
-                    print("soup: " + url.get('root_url') + src)
-                    
-                    if (not soup.findAll('a', class_="media mediafile mf_pdf")):
-                        bot.send_message(_chat_id, "Нет файлов")
-                    else:
-                        for var in soup.findAll('a', class_="media mediafile mf_pdf"):
-                            _n = var["title"]
-                            _h = var['href']
-                            _n = ('.pdf'.join(_n.split('.pdf')[:-1]) + '.pdf').replace('_', ' ').replace('corp', '').replace('pld', '').replace('_', ' ')
-                            f = open(save_dir + _n, "wb")
-                            r = s.get(url.get('root_url') + _h, headers = {'User-Agent': user_agent_val})
-                            print(r)
-                            f.write(r.content)
-                            f.close()
-                            doc = open(save_dir + _n, 'rb')
-                            bot.send_document(_chat_id, doc)
-                        
+                    c = search_files(src)
+                    _files = parse_pdf(c)
                     request = {_chat_id : False}
+                    
+                    if (len(_files) == 0):
+                        bot.reply_to(message, "Нет файлов")
+                    else:
+                        _count = len(_files) // 10
+                        bot.reply_to(message, "Файлов нашлось: " + str(len(_files)))
+                        
+                        for x in range(_count + 1):
+                            bot.send_media_group(_chat_id, [telebot.types.InputMediaDocument(open(doc, 'rb')) for doc in _files[x*10:x*10+10]])
+                            time.sleep(wait_time)
+
                 elif (num > 1):
                     msg += "\nКакой номер интересует?"
-                    bot.send_message(_chat_id, msg)
+                    bot.reply_to(message, msg)
                     request = {_chat_id : True}
                     request_str = {_chat_id : svars}
                     request_num = {_chat_id : num}
@@ -139,65 +180,36 @@ def pld(message):
             elif (_command.isdigit() and request.get(_chat_id) and (int(_command)-1 <= request_num.get(_chat_id)) and (int(_command)-1 >= 0)):
                 request = {_chat_id : False}
                 keys = list(request_str.get(_chat_id).keys())
-                #bot.reply_to(message, request_str.get(_chat_id).get(keys[int(_command)-1]))
-                
                 src = request_str.get(_chat_id).get(keys[int(_command)-1])
-                    
-                s = requests.Session()
-                r = s.get(url.get('root_url'), headers = {'User-Agent': user_agent_val})
-                print ("set User Agent: "+user_agent_val)
-                
-                _cookie = s.cookies.get(url.get('cookie'), domain=url.get('host'))
-                s.headers.update({'Referer':url.get('ref_url_ref')})
-                s.headers.update({'User-Agent':user_agent_val})
-                print ("set " + url.get('cookie')+" to "+str(_cookie))
-                       
-                r = s.get(url.get('auth_page'), headers = {'User-Agent': user_agent_val})
-                print ("try to auth on "+url.get('auth_page'))
-                
-                post_request = s.post(url.get('auth_page'), {
-                'sectok' : '', 
-                'id' : 'start',
-                'do' : 'login',
-                'u' : login,
-                'p' : password,
-                'r' : 1
-                })
-                    
-                r = s.get(url.get('root_url') + src, headers = {'User-Agent': user_agent_val})
-                
-                c = r.content
-                soup = BeautifulSoup(c,'lxml')
-                
-                print("soup: " + url.get('root_url') + src)
-                
-                if (not soup.findAll('a', class_="media mediafile mf_pdf")):
-                    bot.send_message(_chat_id, "Нет файлов")
-                else:
-                    for var in soup.findAll('a', class_="media mediafile mf_pdf"):
-                        _n = var["title"]
-                        _h = var['href']
-                        _n = ('.pdf'.join(_n.split('.pdf')[:-1]) + '.pdf').replace('_', ' ').replace('corp', '').replace('pld', '')
-                        f = open(save_dir + _n, "wb")
-                        r = s.get(url.get('root_url') + _h, headers = {'User-Agent': user_agent_val})
-                        print(r)
-                        f.write(r.content)
-                        f.close()
-                        doc = open(save_dir + _n, 'rb')
-                        bot.send_document(_chat_id, doc)
-                
+                c = search_files(src)
+                _files = parse_pdf(c)
                 request = {_chat_id : False}
+                
+                if (len(_files) == 0):
+                    bot.reply_to(message, "Нет файлов")
+                else:
+                    _count = len(_files) // 10
+                    bot.reply_to(message, "Файлов нашлось: " + str(len(_files)))
+                    
+                    for x in range(_count + 1):
+                        bot.send_media_group(_chat_id, [telebot.types.InputMediaDocument(open(doc, 'rb')) for doc in _files[x*10:x*10+10]])
+                        time.sleep(wait_time)
                 
             else:
                 pass
         else:
-            print (_chat_id)
+            msg = 'UNAUTORIZED ACCESS ATTEMP from '+str(_chat_id)
+            logging.warning(msg)
+            
     except Exception as e:
-            print (e)
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            #logging.error(exc_type, fname, exc_tb.tb_lineno)
+        print (e)
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        logging.error(exc_type, fname, exc_tb.tb_lineno)
+        try:
             bot.reply_to(message, e)
+        except:
             pass
-        
+        pass
+
 bot.polling(none_stop = True)
